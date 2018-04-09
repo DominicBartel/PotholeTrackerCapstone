e5a79de33c90ffd21f083474fdbc224676af77ee
Kathy Walker <kjata1013@gmail.com>
2018-04-09 14:43:46 -0400

    created preliminary test files

---
+++ Capstone.Tests/PotholeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Tests.Pothole_Tests
+{
+	[TestClass]
+	public class PotholeTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ Capstone.Tests/UserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Tests
+{
+	[TestClass]
+	public class UserTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
