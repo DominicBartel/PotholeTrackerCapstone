f88f0a85e6b60b6ebbd7dde9bd7d92c49021ab72
John Slike <jslike1@kent.edu>
2018-04-09 12:16:36 -0400

    updated model and completed GetAllValidPotholes Method

--- Capstone.Web/DAL/PotholeSqlDAL.cs
+++ Capstone.Web/DAL/PotholeSqlDAL.cs
@@ -32,13 +32,35 @@ namespace Capstone.Web.DAL
 
 
                     SqlCommand cmd = new SqlCommand("SELECT * FROM PotHole");
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        Pothole p = new Pothole();
+
+                        p.PotholeId = Convert.ToInt32(reader["PotHole_Id"]);
+                        p.UserId = (Guid)reader["UserId"];
+                        p.PotholeDesc = Convert.ToString(reader["PotHoleDesc"]);
+                        p.Latitude = Convert.ToDouble(reader["Lat"]);
+                        p.Longitude = Convert.ToDouble(reader["Long"]);
+                        p.Severity = Convert.ToInt32(reader["Severity"]);
+                        p.Street1 = Convert.ToString(reader["Street1"]);
+                        p.Street2 = Convert.ToString(reader["Street2"]);
+                        p.LocationDesc = Convert.ToString(reader["LocationDesc"]);
+                        p.ReportedDate = Convert.ToDateTime(reader["DateReported"]);
+                        p.InspectedDate = Convert.ToDateTime(reader["InspectedDate"]);
+                        p.RepairedDate = Convert.ToDateTime(reader["RepairDate"]);
+                        p.IsValidated = Convert.ToBoolean(reader["IsValidated"]);
+
+                        allPotholes.Add(p);
+                    }
                 }
             }
             catch (SqlException ex)
             {
                 throw;
             }
-
 
             return allPotholes;
         }
