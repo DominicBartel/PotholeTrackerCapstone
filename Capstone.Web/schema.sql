50c98a9fe3fb8dfb857f2e8090818d6de1543258
Kathy Walker <kjata1013@gmail.com>
2018-04-09 11:10:47 -0400

    schema first draft completed for review

--- Capstone.Web/schema.sql
+++ Capstone.Web/schema.sql
@@ -1,7 +1,10 @@
-﻿USE PotHoleDB;
+﻿CREATE DATABASE PotHoleDB
 
 GO
 
+USE PotHoleDB;
+
+GO
 
 CREATE TABLE [dbo].[Users]
 (
@@ -19,4 +22,30 @@ CREATE TABLE [dbo].[UserRoles]
 	CONSTRAINT pk_UserRoles PRIMARY KEY (UserId, Role),
 	CONSTRAINT fk_UserRoles_Users FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
+
+CREATE TABLE [dbo].[RepairOrder]
+(
+	[RepairId]	UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[Complete]	BIT				 NULL
+
+);
  
+ CREATE TABLE [dbo].[PotHole]
+ (
+   [PotHole_Id]		UNIQUEIDENTIFIER	NOT NULL PRIMARY KEY,
+   [UserId]			UNIQUEIDENTIFIER	NOT NULL,
+   [RepairId]		UNIQUEIDENTIFIER	NOT NULL,
+   [PotHoleDesc]	VARCHAR(100)		NULL,
+   [Lat]			FLOAT(50)			NULL,
+   [Long]			FLOAT(50)			NULL,
+   [Severity]		TINYINT				NULL,
+   [Street1]		VARCHAR(50)			NULL,
+   [Street2]		VARCHAR(50)			NULL,
+   [DateReported]	DATETIME			NULL,
+   [InspectedDate]	DATETIME			NULL,
+   [RepairDate]		DATETIME			NULL,
+   [IsValidated]	BIT					NULL,
+
+   CONSTRAINT fk_UserId FOREIGN KEY (UserId) REFERENCES Users(UserId),
+   CONSTRAINT fk_RepairId FOREIGN KEY (RepairId) REFERENCES RepairOrder(RepairId),
+ );
