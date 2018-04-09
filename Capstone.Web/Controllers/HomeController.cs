5bc4361a0f4dbbfa314f9ed8de6c81458c709e66
John Slike <jslike1@kent.edu>
2018-04-09 11:08:45 -0400

    added dependency injection, configured ninject, added GetAllPotholes method

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Web.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,15 @@ namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        public IPotholeDAL potholeDAL;
+
+        public HomeController(IPotholeDAL dal)
+        {
+            this.potholeDAL = dal;
+        }
+
+
         public ActionResult Index()
         {
             return View();
--- Capstone.Web/DAL/IPotholeDAL.cs
+++ Capstone.Web/DAL/IPotholeDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,10 @@ using System.Threading.Tasks;
 
 namespace Capstone.Web.DAL
 {
-    interface IPotholeDAL
+    public interface IPotholeDAL
     {
-
+        List<Pothole> GetAllPotholes();
+        
 
 
 
